a663da9a89867e6257fe9ec71506b5faff883037
masaya-takasaka <takasakamasaya@gmail.com>
2021-04-26 03:54:13 +0900

    バリデーション

--- db/migrate/20210425172812_devise_create_users.rb
+++ db/migrate/20210425172812_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false, default: ""
+      t.string :email,              null: false, default: "", uniqueness: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :ruby_last_name,     null: false, default: ""
+      t.string :ruby_first_name,    null: false, default: ""
+      t.date   :birthdate,          null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
