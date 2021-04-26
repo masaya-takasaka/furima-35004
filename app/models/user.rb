3cdeef025898bea78f1a0b43e626bdcd4606ded1
masaya-takasaka <takasakamasaya@gmail.com>
2021-04-26 14:51:28 +0900

    バリデーション修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+　VALIDA_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+　VALID_PASSWORD_REGEX　=/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/
+
+  validates　:nickname,       presence: true
+  validates  :email,          {presence: true,  ,  format: { with: VALID_EMAIL_REGEX },   uniqueness: { case_sensitive: false }}
+  validates  :password,       {presence: true,  ,  format: { with: VALID_PASSWORD_REGEX}}
 end
